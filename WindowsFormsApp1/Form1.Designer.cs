453badaf224c1f4bbbab7e2616a871b0f1a44550
Anton Buketov <antonbuk@ut.ee>
2022-11-13 16:37:24 +0200

    ready

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,99 @@
+﻿namespace Number_Guessing_Game
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(46, 85);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(185, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(99, 133);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Check";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseClick += new System.Windows.Forms.MouseEventHandler(this.CheckNumber);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(43, 36);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(296, 17);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "I am thinking of a number between: 0 and 100";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(46, 201);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(144, 17);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "You guessed  0 times";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(400, 253);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtEnterNumber;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Number_Guessing_Game
+{
+    public partial class Form1 : Form
+    {
+
+        Random randomNumber = new Random();
+
+        int number = 0;
+        int guesses = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            loadQuestions();
+        }
+
+        private void CheckNumber(object sender, EventArgs e)
+        {
+          
+
+        }
+
+        private void loadQuestions()
+        {
+            number = randomNumber.Next(0, 100);
+
+            label3.Text = "I am thinking of a number between: 0 and 100 ";
+        }
+
+ 
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CheckNumber(object sender, MouseEventArgs e)
+        {
+            int i = Convert.ToInt32(textBox1.Text);
+
+            guesses += 1;
+            label4.Text = "You guessed " + guesses + " times";
+
+            if (i == number)
+            {
+                MessageBox.Show("Nice, you guessed it. Try another");
+                loadQuestions();
+                textBox1.Text = "";
+                guesses = 0;
+                label4.Text = "You guessed " + guesses + " times";
+            }
+            else if (i < number)
+            {
+                MessageBox.Show("Go Higher");
+            }
+            else
+            {
+                MessageBox.Show("Go Lower");
+            }
+
+        }
+    }
+}
